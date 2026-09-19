ec99b2d4f0e27aa4f9f57658ddf9a3d02c61ef0c
yashpatil12345678 <yashhp050205@gmail.com>
2026-09-19 19:16:32 +0530

    Add FoundationPage component to section page

--- app/[section]/page.tsx
+++ app/[section]/page.tsx
@@ -1,2 +1,3 @@
 import { FoundationPage } from '@/components/foundation-page'
 export default async function Page({params}:{params:Promise<{section:string}>}){const {section}=await params;return <FoundationPage section={section}/>} 
+
